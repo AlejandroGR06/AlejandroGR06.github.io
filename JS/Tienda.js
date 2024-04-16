42d72eb92e517c1dbd3ff05a8f2dd53339523a83
AlejandroGR06 <167254597+AlejandroGR06@users.noreply.github.com>
2024-04-17 00:55:06 +0200

    PrimerSummit

---
+++ JS/Tienda.js
@@ -0,0 +1,7 @@
+<script>
+function agregarAlCarrito(nombreProducto, precioProducto) {
+    // Supongamos que aquí hay una lógica para agregar el producto al carrito
+    alert(`Se agregó "${nombreProducto}" al carrito por $${precioProducto}`);
+}
+</script>
+
